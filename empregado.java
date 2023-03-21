1856bb58a2d399e4a17dcd3dd3e9b883dc97e77f
Guilherme Oliver <guilhermebarreira.0805@aluno.saojudas.br>
2023-03-20 21:25:57 -0300

    Adicionando o arquivo .gitignore

--- empregado.java
+++ empregado.java
@@ -32,18 +32,58 @@ public class Empregado {
         this.idade = idade;
     }
 
-
-    public int getTipo(){
+    public int getTipo() {
         return this.tipo;
     }
 
-    public void setTipo(int tipo){
+    public void setTipo(int tipo) {
         this.tipo = tipo;
     }
-    
+
+    public double getSalario() {
+        return this.salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public double getComissao() {
+        return this.comissao;
+    }
+
+    public void setComissao(double comissao) {
+        this.comissao = comissao;
+    }
+
+    public double getBonus() {
+        return this.bonus;
+    }
+
+    public void setBonus(double bonus) {
+        this.bonus = bonus;
+    }
+
 }
-    public class TesteEmpregado {
-        public static void main(String[] args){
-        
+
+public class TesteEmpregado {
+    public static void main(String[] args){
+        Empregado e1 = new Empregado();
+        Empregado e2 = new Empregado();
+        Empregado e3 = new Empregado();
+        //configurando tipo dos empregados
+        e1.setTipo(1);
+        e2.setTipo(2);
+        e3.setTipo(3);
+        //configurando alguns valores
+        e1.setSalario(2000);
+        e2.setSalario(1700);
+        e2.setComissao(0.2); //20% de comissão
+        e3.setSalario(1500);
+        e3.setBonus(400);
+        //mostra valores
+        System.out.println(e1.calculaSalario());
+        System.out.println(e2.calculaSalario());
+        System.out.println(e3.calculaSalario());
     }
 }
--- testeEmpregado.java
+++ testeEmpregado.java
@@ -1,5 +1,5 @@
 public class testeEmpregado{
-    public public static void main(String[] args) {
+    public static void main(String[] args) {
         Empregado e1 = new Empregado();
         Empregado e2 = new Empregado();
         Empregado e3 = new Empregado();
